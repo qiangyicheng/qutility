eb35abc24885551472c942ce9e9ece2bb2ee80e7
qiang yicheng <qiangyicheng@hotmail.com>
2020-09-21 16:16:58 +0800

    现在ArrayHBW和ArrayDDR继承自各自的DArray; ArrayCPU现在提供统一的实现

--- qutility/array_wrapper/array_wrapper_cpu.h
+++ qutility/array_wrapper/array_wrapper_cpu.h
@@ -3,58 +3,22 @@
 #include <cstdio>
 #include <vector>
 #include "boost/align.hpp"
-
+#include "detail.h"
 #include "hbw_posix_allocator.h"
 
 namespace qutility {
 	namespace array_wrapper {
 
-		class ArrayCPUBase { };
+		class ArrayCPUBase {};
 
-		template <class T, std::size_t S, std::size_t A = 64>
-		class ArrayDDR {
+		template<class T, class Allocator, size_t A>
+		class ArrayCPU : ArrayCPUBase {
 		public:
-			ArrayDDR() : data_(S, T()), pointer_(&(data_.at(0))) {	}
-			ArrayDDR(const T& val) : data_(S, val), pointer_(&(data_.at(0))) {	}
-
-			constexpr static std::size_t Size = S;
-			constexpr static std::size_t Alignment = A;
-
-			inline const T* pointer() const { return pointer_; }
-			inline T* pointer() { return pointer_; }
-			inline const T& operator[](size_t pos) const { return data_[pos]; }
-			inline T& operator[](size_t pos) { return data_[pos]; }
-
-		private:
-			std::vector<T, boost::alignment::aligned_allocator<T, Alignment>> data_;
-			T* const pointer_;
-		};
-
-		template <class T, std::size_t S, std::size_t A = 64>
-		class ArrayHBW {
-		public:
-			ArrayHBW() : data_(S, T()), pointer_(&(data_.at(0))) { }
-			ArrayHBW(const T& val) : data_(S, val), pointer_(&(data_.at(0))) { }
-
-			constexpr static std::size_t Size = S;
-			constexpr static std::size_t Alignment = A;
-
-			inline const T* pointer() const { return pointer_; }
-			inline T* pointer() { return pointer_; }
-			inline const T& operator[](size_t pos) const { return data_[pos]; }
-			inline T& operator[](size_t pos) { return data_[pos]; }
-
-		private:
-			std::vector<T, hbw::allocator<T, Alignment>> data_;
-			T* const pointer_;
-		};
-
-		template <class T, std::size_t A = 64>
-		class DArrayDDR : ArrayCPUBase {
-		public:
-			DArrayDDR() = delete;
-			DArrayDDR(std::size_t S) : data_(S, T()), size_(S), pointer_(&(data_.at(0))) {	}
-			DArrayDDR(const T& val, std::size_t S) : data_(S, val), size_(S), pointer_(&(data_.at(0))) {	}
+			ArrayCPU() = delete;
+			ArrayCPU(std::size_t S) : data_(S, T()), size_(S), pointer_(&(data_.at(0))) {	}
+			ArrayCPU(const T& val, std::size_t S) : data_(S, val), size_(S), pointer_(&(data_.at(0))) {	}
+			template<typename OtherT, typename OtherAlloc>
+			ArrayCPU(const std::vector<OtherT, OtherAlloc>& v, std::size_t S) : data_(detail::duplicate(v, S)), size_(S), pointer_(&(data_.at(0))) {}
 			operator T* () { return pointer_; }
 			operator const T* () const { return pointer_; }
 			T* operator+(size_t shift) { return pointer_ + shift; }
@@ -69,33 +33,37 @@ namespace qutility {
 			inline T& operator[](size_t pos) { return data_[pos]; }
 
 		private:
-			std::vector<T, boost::alignment::aligned_allocator<T, Alignment>> data_;
+			std::vector<T, Allocator> data_;
 			T* const pointer_;
 		};
 
 		template <class T, std::size_t A = 64>
-		class DArrayHBW : ArrayCPUBase {
+		using DArrayDDR = ArrayCPU<T, boost::alignment::aligned_allocator<T, Alignment>, A>;
+
+		template <class T, std::size_t A = 64>
+		using DArrayHBW = ArrayCPU<T, hbw::allocator<T, Alignment>, A>;
+
+		template <class T, std::size_t S, std::size_t A = 64>
+		class ArrayDDR : public DArrayDDR<T, A> {
 		public:
-			DArrayHBW() = delete;
-			DArrayHBW(std::size_t S) : data_(S, T()), size_(S), pointer_(&(data_.at(0))) { }
-			DArrayHBW(const T& val, std::size_t S) : data_(S, val), size_(S), pointer_(&(data_.at(0))) { }
-			operator T* () { return pointer_; }
-			operator const T* () const { return pointer_; }
-			T* operator+(size_t shift) { return pointer_ + shift; }
-			const T* operator+(size_t shift) const { return pointer_ + shift; }
+			ArrayDDR() : DArrayDDR<T, A>(S) {	}
+			ArrayDDR(const T& val) : DArrayDDR<T, A>(val, S) {	}
+			template<typename OtherT, typename OtherAlloc>
+			ArrayDDR(const std::vector<OtherT, OtherAlloc>& v) : DArrayDDR<T, A>(v, S) {}
 
-
-			const std::size_t size_;
-			constexpr static std::size_t Alignment = A;
-
-			inline const T* pointer() const { return pointer_; }
-			inline T* pointer() { return pointer_; }
-			inline const T& operator[](size_t pos) const { return data_[pos]; }
-			inline T& operator[](size_t pos) { return data_[pos]; }
-
-		private:
-			std::vector<T, hbw::allocator<T, Alignment>> data_;
-			T* const pointer_;
+			constexpr static std::size_t Size = S;
 		};
+
+		template <class T, std::size_t S, std::size_t A = 64>
+		class ArrayHBW : public DArrayHBW<T, A> {
+		public:
+			ArrayHBW() : DArrayHBW<T, A>(S) { }
+			ArrayHBW(const T& val) : DArrayHBW<T, A>(val, S) { }
+			template<typename OtherT, typename OtherAlloc>
+			ArrayHBW(const std::vector<OtherT, OtherAlloc>& v) : DArrayHBW<T, A>(v, S) {}
+
+			constexpr static std::size_t Size = S;
+		};
+
 	}
 }
