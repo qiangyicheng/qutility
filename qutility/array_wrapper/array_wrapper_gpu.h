e8619a5133cca6c63f2150fdee5d19e0f7d3490c
qiang yicheng <qiangyicheng@hotmail.com>
2020-09-21 15:46:33 +0800

    现在ArrayGPU和ArrayDDRPinned继承自对应的DArray

--- qutility/array_wrapper/array_wrapper_gpu.h
+++ qutility/array_wrapper/array_wrapper_gpu.h
@@ -21,64 +21,6 @@ namespace qutility {
 
 		//Allocate memory in GPU
 		//Note that it is done through a thrust interface.
-		template <class T, std::size_t S>
-		class ArrayGPU :public ArrayGPUBase {
-		public:
-			ArrayGPU() = delete;
-			ArrayGPU(int device) : ArrayGPUBase(device), data_(S, T()), pointer_(thrust::raw_pointer_cast(&(data_[0]))) {}
-			ArrayGPU(const T& val, int device) : ArrayGPUBase(device), data_(S, val), pointer_(thrust::raw_pointer_cast(&(data_[0]))) {}
-			template<typename OtherT, typename OtherAlloc>
-			ArrayGPU(const std::vector<OtherT, OtherAlloc>& v, int device) : ArrayGPUBase(device), data_(detail::duplicate(v, S)), pointer_(thrust::raw_pointer_cast(&(data_[0]))) {}
-			operator T* () { return pointer_; }
-			operator const T* () const { return pointer_; }
-			T* operator+(size_t shift) { return pointer_ + shift; }
-			const T* operator+(size_t shift) const { return pointer_ + shift; }
-
-			constexpr static std::size_t Size = S;
-
-			const T* pointer() const { return pointer_; }
-			T* pointer() { return pointer_; }
-			inline const auto operator[](size_t pos) const { return data_[pos]; }
-			inline auto operator[](size_t pos) { return data_[pos]; }
-
-		private:
-			thrust::device_vector<T> data_;
-			T* const pointer_;
-		};
-
-		template <class T, std::size_t S, std::size_t A = 64>
-		class ArrayDDRPinned {
-		public:
-			ArrayDDRPinned() : data_(S, T()), pointer_(&(data_.at(0))) {
-				cudaHostRegister((void*)pointer_, Size * sizeof(T), cudaHostRegisterDefault);
-			}
-			ArrayDDRPinned(const T& val) : data_(S, val), pointer_(&(data_.at(0))) {
-				cudaHostRegister((void*)pointer_, Size * sizeof(T), cudaHostRegisterDefault);
-			}
-			~ArrayDDRPinned() {
-				cudaHostUnregister((void*)pointer_);
-			}
-			operator T* () { return pointer_; }
-			operator const T* () const { return pointer_; }
-			T* operator+(size_t shift) { return pointer_ + shift; }
-			const T* operator+(size_t shift) const { return pointer_ + shift; }
-
-			constexpr static std::size_t Size = S;
-			constexpr static std::size_t Alignment = A;
-
-			inline const T* pointer() const { return &(data_.at(0)); }
-			inline T* pointer() { return &(data_.at(0)); }
-			inline const T& operator[](size_t pos) const { return data_[pos]; }
-			inline T& operator[](size_t pos) { return data_[pos]; }
-
-		private:
-			std::vector<T, boost::alignment::aligned_allocator<T, Alignment>> data_;
-			T* const pointer_;
-		};
-
-
-		//Allocate memory in GPU
-		//Note that it is done through a thrust interface.
 		template <class T>
 		class DArrayGPU :public ArrayGPUBase {
 		public:
@@ -100,6 +42,18 @@ namespace qutility {
 		private:
 			thrust::device_vector<T> data_;
 			T* const pointer_;
+		};
+
+		template <class T, std::size_t S>
+		class ArrayGPU : public DArrayGPU<T> {
+		public:
+			ArrayGPU() = delete;
+			ArrayGPU(int device) :DArrayGPU<T>(S, device) {}
+			ArrayGPU(const T& val, int device) : DArrayGPU<T>(val, S, device) {}
+			template<typename OtherT, typename OtherAlloc>
+			ArrayGPU(const std::vector<OtherT, OtherAlloc>& v, int device) : DArrayGPU<T>(v, S, device) {}
+
+			constexpr static std::size_t Size = S;
 		};
 
 		template <class T, std::size_t A = 64>
@@ -131,6 +85,16 @@ namespace qutility {
 		private:
 			std::vector<T, boost::alignment::aligned_allocator<T, Alignment>> data_;
 			T* const pointer_;
+		};
+
+		template <class T, std::size_t S, std::size_t A = 64>
+		class ArrayDDRPinned : public DArrayDDRPinned<T, A> {
+		public:
+			ArrayDDRPinned() : DArrayDDRPinned<T, A>(S) {}
+			ArrayDDRPinned(const T& val) : DArrayDDRPinned<T, A>(val, S) {}
+			~ArrayDDRPinned() {	}
+
+			constexpr static std::size_t Size = S;
 		};
 
 	}
